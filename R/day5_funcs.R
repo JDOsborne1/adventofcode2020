6a5756365bfe7946fd70fe54260a30b27b71c49d
User <user@Users-MacBook-Air.local>
2020-12-05 11:32:43 +0000

    Including function to split a vector based on its median and input args

--- R/day5_funcs.R
+++ R/day5_funcs.R
@@ -2,3 +2,12 @@ aoc_day5_parse_input <- function(input_ds){
         input_ds  %>%
                 rename(pass_code = X1)
 }
+
+aoc_day5_split_vector <- function(input_vector, switch_arg){
+        median_loc <- median(input_vector)
+        if(switch_arg %in% c("B", "R")){
+                input_vector[input_vector > median_loc]
+        } else {
+                input_vector[input_vector < median_loc]
+        }
+}
--- tests/testthat/test-day5_funcs.R
+++ tests/testthat/test-day5_funcs.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("vector splitting works", {
+        expect_equal(aoc_day5_split_vector(0:127, "F"), 0:63)
+        expect_equal(aoc_day5_split_vector(0:127, "B"), 64:127)
+        expect_equal(aoc_day5_split_vector(0:7, "L"), 0:3)
+        expect_equal(aoc_day5_split_vector(0:7, "R"), 4:7)
+
 })
